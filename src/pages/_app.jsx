8e4ca0731696c8165b9169c9835fea7465c511f7
Mohamad Kassas <mkass090@uottawa.ca>
2023-03-26 03:18:13 -0400

    "Quick fix?"

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,7 @@
 import "@/styles/globals.css";
 import LandingPage from "./Components/Landing Page/landingPage";
-import Popup from "./Components/Popup/popup";
 
 export default function App({ Component, pageProps }) {
   return (
-  // <Popup login={false} signUp={false} employeePopup={true}/>
-  <LandingPage />);
+    <LandingPage />);
 }
